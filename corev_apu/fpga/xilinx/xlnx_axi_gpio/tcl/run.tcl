4b8180aa5eb5501b804a50a2ce58280e4a1924ca
Muhammad Ammar LUKMAN HAKIM <muhammad-ammar.lukman-hakim@grenoble-inp.org>
2025-07-17 20:59:43 +0200

    Configure IP cores for CW310 board

--- corev_apu/fpga/xilinx/xlnx_axi_gpio/tcl/run.tcl
+++ corev_apu/fpga/xilinx/xlnx_axi_gpio/tcl/run.tcl
@@ -7,8 +7,12 @@ create_project $ipName . -force -part $partNumber
 set_property board_part $boardName [current_project]
 
 create_ip -name axi_gpio -vendor xilinx.com -library ip -module_name $ipName
-set_property -dict [list CONFIG.C_GPIO_WIDTH {8} CONFIG.C_GPIO2_WIDTH {8} CONFIG.C_IS_DUAL {1} CONFIG.C_ALL_INPUTS_2 {1} CONFIG.C_INTERRUPT_PRESENT {0}] [get_ips $ipName]
 
+if {$::env(BOARD) eq "bergen"} {
+set_property -dict [list CONFIG.C_GPIO_WIDTH {9} CONFIG.C_GPIO2_WIDTH {8} CONFIG.C_IS_DUAL {1} CONFIG.C_ALL_INPUTS_2 {1} CONFIG.C_INTERRUPT_PRESENT {0}] [get_ips $ipName]
+} else {
+set_property -dict [list CONFIG.C_GPIO_WIDTH {8} CONFIG.C_GPIO2_WIDTH {8} CONFIG.C_IS_DUAL {1} CONFIG.C_ALL_INPUTS_2 {1} CONFIG.C_INTERRUPT_PRESENT {0}] [get_ips $ipName]
+}
 generate_target {instantiation_template} [get_files ./$ipName.srcs/sources_1/ip/$ipName/$ipName.xci]
 generate_target all [get_files  ./$ipName.srcs/sources_1/ip/$ipName/$ipName.xci]
 create_ip_run [get_files -of_objects [get_fileset sources_1] ./$ipName.srcs/sources_1/ip/$ipName/$ipName.xci]
--- corev_apu/fpga/xilinx/xlnx_clk_gen/tcl/run.tcl
+++ corev_apu/fpga/xilinx/xlnx_clk_gen/tcl/run.tcl
@@ -9,13 +9,28 @@ set_property board_part $boardName [current_project]
 create_ip -name clk_wiz -vendor xilinx.com -library ip -module_name $ipName
 
 if {$::env(BOARD) eq "nexys_video"} {
-    set_property -dict [list CONFIG.PRIM_IN_FREQ {100.000} \
+set_property -dict [list CONFIG.PRIM_IN_FREQ {100.000} \
                         CONFIG.NUM_OUT_CLKS {5} \
                         CONFIG.CLKOUT2_USED {true} \
                         CONFIG.CLKOUT3_USED {true} \
                         CONFIG.CLKOUT4_USED {true} \
                         CONFIG.CLKOUT5_USED {true} \
                         CONFIG.CLKOUT1_REQUESTED_OUT_FREQ {25} \
+                        CONFIG.CLKOUT2_REQUESTED_OUT_FREQ {125} \
+                        CONFIG.CLKOUT3_REQUESTED_OUT_FREQ {125} \
+                        CONFIG.CLKOUT3_REQUESTED_PHASE {90.000} \
+                        CONFIG.CLKOUT4_REQUESTED_OUT_FREQ {50} \
+                        CONFIG.CLKOUT5_REQUESTED_OUT_FREQ {200} \
+                        CONFIG.CLKIN1_JITTER_PS {50.0} \
+                       ] [get_ips $ipName]
+} elseif {$::env(BOARD) eq "bergen"}  {
+set_property -dict [list CONFIG.PRIM_IN_FREQ {100.000} \
+                        CONFIG.NUM_OUT_CLKS {5} \
+                        CONFIG.CLKOUT2_USED {true} \
+                        CONFIG.CLKOUT3_USED {true} \
+                        CONFIG.CLKOUT4_USED {true} \
+                        CONFIG.CLKOUT5_USED {true} \
+                        CONFIG.CLKOUT1_REQUESTED_OUT_FREQ {50} \
                         CONFIG.CLKOUT2_REQUESTED_OUT_FREQ {125} \
                         CONFIG.CLKOUT3_REQUESTED_OUT_FREQ {125} \
                         CONFIG.CLKOUT3_REQUESTED_PHASE {90.000} \
